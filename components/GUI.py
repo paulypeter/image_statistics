d5434355cccfef0a81a15c0ba5c4a500531b2cde
Peter Pauly <p.pauly@tu-bs.de>
2021-06-22 16:23:34 +0200

    menubar for GUI

--- components/GUI.py
+++ components/GUI.py
@@ -1,2 +1,9 @@
 import tkinter as tk
+from tkinter import ttk
 
+def create_window() -> tk.Tk():
+    main_window = tk.Tk()
+    menubar = tk.Menu(main_window)
+
+    main_window.config(menu=menubar)
+    return main_window
