dbb320600dfe7b1cb3625ccc3a234f71cbb04551
Peter Pauly <p.pauly@tu-bs.de>
2021-06-24 20:07:21 +0200

    empty pie_chart()

--- components/charts.py
+++ components/charts.py
@@ -4,3 +4,7 @@ from matplotlib import pyplot as plt
 def bar_chart(input_dict, x_label, y_label):
     chart = plt.figure()
     return chart
+
+def pie_chart(input_dict, x_label, y_label):
+    chart = plt.figure()
+    return chart
